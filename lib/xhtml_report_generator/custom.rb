267e64b191d359db863c0aceeaf9bb5b7c0699cf
Manuel Widmer <m-widmer@gmx.ch>
2014-12-18 16:20:36 +0100

    added highlightCaptures function that enables the highlighting of captures of a regexp only.

--- lib/xhtml_report_generator/custom.rb
+++ lib/xhtml_report_generator/custom.rb
@@ -62,10 +62,14 @@ module Custom
   def contentBefore(locaiton, text)
   end
 
-  def highlight(regex, color="y", el = @current)
+  # puts a <span> </span> tag around all captures of the regex
+  # NOTE: nested captures are not supported and don't make sense in this context!!
+  # @param regex [Regexp] a regular expression that will be matched
+  # @param color [String] at this point one of "y", "r", "g", "b" (yellow, red, green, blue) is supported
+  # @param el [REXML::Element] the Element (scope) which will be searched for pattern matches
+  def highlightCaptures(regex, color="y", el = @current)
     # get all children of the current node
     arr = el.to_a()
-
     # depth first recursion into grand-children
     for i in arr do
       # detach from current
@@ -73,40 +77,54 @@ module Custom
       if i.class.to_s()  == "REXML::Text"
         # in general a text looks as follows:
         # .*(matchstring|.*)*
+
+        # We get an array of [[start,length], [start,length], ...] for all our regex SUB-matches
+        positions = i.value().enum_for(:scan, regex).flat_map {
+          # Regexp.last_match is a MatchData object, the index 0 is the entire match and
+          # indices 1..n are the captures (sub expressions)
+          array = Array.new
+          for k in 1..Regexp.last_match.length - 1 do
+            array.push([Regexp.last_match.begin(k),
+              Regexp.last_match.end(k)-Regexp.last_match.begin(k)])
+          end
+          array
+        }
+        replaceTextWithElements(el, i, "span", {"class" => color}, positions)
+      else
+        # for non-text nodes we recurse into it and finally reattach to our parent to preserve ordering
+        highlight(regex, color, i)
+        el.add(i)
+      end # if  i.class.to_s()  == "REXML::Text"
+    end # for i in arr do
+
+  end
+
+  # puts a <span> </span> tag around all matches of regex
+  # @param regex [Regexp] a regular expression that will be matched
+  # @param color [String] at this point one of "y", "r", "g", "b" (yellow, red, green, blue) is supported
+  # @param el [REXML::Element] the Element (scope) which will be searched for pattern matches
+  def highlight(regex, color="y", el = @current)
+    # get all children of the current node
+    arr = el.to_a()
+    #puts arr.inspect
+    # depth first recursion into grand-children
+    for i in arr do
+      # detach from current
+      i.parent = nil
+      #puts i.class.to_s()
+      if i.class.to_s()  == "REXML::Text"
+        # in general a text looks as follows:
+        # .*(matchstring|.*)*
+
         # We get an array of [[start,length], [start,length], ...] for all our regex matches
         positions = i.value().enum_for(:scan, regex).map {
           [Regexp.last_match.begin(0),
             Regexp.last_match.end(0)-Regexp.last_match.begin(0)]
         }
-
-        last_end = 0
-        index = 0
-        for j in positions do
-          # reattach normal (unmatched) text
-          if j[0] > last_end
-            text = REXML::Text.new(i.value()[ last_end, j[0] - last_end ])
-            el.add_text(text)
-          end
-          #create the span node with color and add the text to it
-          span = el.add_element(REXML::Element.new("span"), {"class" => color})
-          span.add_text(i.value()[ j[0], j[1] ])
-          last_end = j[0]+j[1]
-          # in the last round check for any remaining text
-          if index == positions.length - 1
-            if last_end < i.value().length
-              text = REXML::Text.new(i.value()[ last_end, i.value().length - last_end ])
-              el.add(text)
-            end
-          end
-          index  += 1
-        end # for j in positions do
-
-        # don't forget to reattach the textnode if there are no regex matches at all
-        if index == 0
-          el.add(i)
-        end
+        replaceTextWithElements(el, i, "span", {"class" => color}, positions)
       else
-        # for non-text nodes we recurse into it and finally reattacht to our parent to preserve ordering
+        # for non-text nodes we recurse into it and finally reattach to our parent to preserve ordering
+        # puts "recurse"
         highlight(regex, color, i)
         el.add(i)
       end # if  i.class.to_s()  == "REXML::Text"
@@ -137,6 +155,48 @@ module Custom
 
     return @current
   end
+
+  #
+  # @param element [REXML::Element] the element in whose text tags will be added at the specified indices of @index_length_array
+  # @param parent [REXML::Element] the parent to which @element should be attached after parsing
+  # @param tagname [String] the tag that will be introduced as <tagname> at the indices specified
+  # @param attribs [Hash] Attributes that will be added to the inserted tag e.g. <tagname attrib="test">
+  # @param index_length_array [Array] Array of the form [[index, lenght], [index, lenght], ...] that specifies
+  #                                   the start position and length of the substring around which the tags will be introduced
+  def replaceTextWithElements(parent, element, tagname, attribs, index_length_array)
+    last_end = 0
+    index = 0
+    #puts index_length_array.inspect
+    #puts element.inspect
+    for j in index_length_array do
+      # reattach normal (unmatched) text
+      if j[0] > last_end
+        text = REXML::Text.new(element.value()[ last_end, j[0] - last_end ])
+        parent.add_text(text)
+      end
+      #create the tag node with attributes and add the text to it
+      tag = parent.add_element(REXML::Element.new(tagname), attribs)
+      tag.add_text(element.value()[ j[0], j[1] ])
+      last_end = j[0]+j[1]
+
+      # in the last round check for any remaining text
+      if index == index_length_array.length - 1
+        if last_end < element.value().length
+          text = REXML::Text.new(element.value()[ last_end, element.value().length - last_end ])
+          parent.add(text)
+        end
+      end
+      index  += 1
+    end # for j in positions do
+
+    # don't forget to reattach the textnode if there are no regex matches at all
+    if index == 0
+      parent.add(element)
+    end
+
+  end
+
+  #private_instance_methods(:replaceTextWithElements)
 
 end
 
--- test/test.rb
+++ test/test.rb
@@ -1,5 +1,7 @@
 require_relative '../lib/xhtml_report_generator'
 
+
+puts "Result"
 gen1 = XhtmlReportGenerator::Generator.new
 #
 gen2 = XhtmlReportGenerator::Generator.new(:custom_rb => "test/custom2.rb")
@@ -8,7 +10,7 @@ gen2.H1
 gen1.createLayout
 gen1.setTitle("Manu's Testreport")
 
-for i in 1..20 do
+for i in 1..10 do
   gen1.heading("h1", "titel #{"Manuel".split("").shuffle.join}", :btoc)
   gen1.heading("h2", "subtitel", :ltoc)
   gen1.heading("h3", "section")
@@ -18,11 +20,13 @@ for i in 1..20 do
   gen1.html("html function: Hallo welt <br /> html test <span class=\"r\" >red span test</span>",{"class"=>"italic"})
 
   # test for nested highlighting
-  gen1.code("some sp fancy cisco < & > an code\nwith newline!\nand sp another second fancy blub an code")
+  gen1.code("some sp fancy cisco < & > an code\nwith newline!\nand sp another second fancy coconut an code")
   
+
   gen1.highlight(/sp.*an/)
-  gen1.highlight(/second.*blub/, "g")
+  gen1.highlight(/second(.*)nut/, "g")
   gen1.highlight(/fancy/, "b")
+  gen1.highlightCaptures(/c(o)d(e)/,"r") 
   
   gen1.content("this is really good lookin'")
   
